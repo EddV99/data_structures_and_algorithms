0defda642b0021af569d690c91d3f5688bf092b9
Eduardo Valdivia <eddval0808@gmail.com>
2025-08-29 16:51:55 -0600

    remove code in main

--- main.c
+++ main.c
@@ -3,21 +3,4 @@
 #include <stdio.h>
 
 int main() {
-  binary_tree_t a = create_binary_tree();
-  a.root = create_tree_node(
-      0,
-      create_tree_node(0, 0, 0, 3),
-      create_tree_node(0, 0, 0, 0x45),
-      5
-      );
-  a.size = 3;
-  binary_tree_t b = create_binary_tree();
-  b.root = create_tree_node(
-      0,
-      create_tree_node(0, 0, 0, 3),
-      create_tree_node(0, 0, 0, 0x45),
-      5
-      );
-  b.size = 3;
-  printf("%s\n", tree_comparison(&a, &b) ? "true" : "false");
 }
